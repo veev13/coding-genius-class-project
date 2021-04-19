f704dc1b184ce36fbafbe0027313a1f3d6f1f8b9
Bittah Swami <41178536+6democratickim9@users.noreply.github.com>
2021-04-19 16:36:55 +0900

    현재까지

---
+++ web_server/rec.py
@@ -0,0 +1,72 @@
+import pymysql
+
+weather = [20, 10, 0, 25, 30, 1, 2, 3, 4, 5]
+
+
+def weather_sort(weather):
+    # 리스트 두 개 받아서 짜기(날씨데이터 5개, 주식데이터 5개)
+    weather_list = []
+    num = 0
+
+    for i in reversed(weather):
+
+        if num < 5:
+            weather_list.append(i)
+            num += 1
+        else:
+            break
+
+    # print(weather_list)
+    weather_rate = []
+
+    for index in range(len(weather_list)):
+        try:
+            prev = weather_list[index]
+            pre = weather_list[index+1]
+            rate = ((prev - pre) / prev)*100
+            weather_rate.append(rate)
+
+        except:
+            break
+
+    return weather_rate
+
+
+def stock_sort(stock_id, price_list):
+    price_re = []
+    for index in price_list:
+        price_re.append(index)
+
+    price_rate = []
+    for i in range(len(price_re)):
+        try:
+            prev = price_re[i]
+            pre = price_re[i+1]
+            rate = ((prev - pre) / prev)*100
+            price_rate.append(rate)
+
+        except:
+            break
+
+    print(price_rate)
+    return price_rate
+
+
+def recommand(weather_rate, price_rate):
+    total = 0
+    for idx in range(len(price_rate)):
+        total = weather_rate[idx] - price_rate[idx]
+    total = round(total, 2)
+    return total
+
+
+def insert_rec(total):
+    db_config = None
+    with open('../config/db_config.txt', 'r') as file:
+        db_config_string = file.read()
+        db_config = loads(db_config_string)
+
+    conn = pymysql.connect(**db_config)
+    cursor = conn.cursor()
+
+    cursor.execute
