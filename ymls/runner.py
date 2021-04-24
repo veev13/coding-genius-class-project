55baaecb0e183cb5e7e80c44c7a3aab5bdd2dbaa
veev23 <veev23@gmail.com>
2021-04-24 17:56:20 +0900

    tmp

--- ymls/runner.py
+++ ymls/runner.py
@@ -5,6 +5,9 @@ import sys
 if sys.argv[1] == '0':
     print("RUN_FILE is '0'!")
     exit(0)
+print(f'{sys.argv[1]} FIND')
+with open("logs.txt",'w') as f:
+    f.write(f"FIND START {sys.argv[1]}")
 while not os.path.isfile(sys.argv[1]):
     time.sleep(5)
 print("RUN!")
