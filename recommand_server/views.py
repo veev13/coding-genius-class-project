38b543649c2ddd55620dc3ab179ba99420011042
choi <boy0516@naver.com>
2021-04-26 20:04:59 +0900

    rec

--- recommand_server/views.py
+++ recommand_server/views.py
@@ -11,11 +11,11 @@ c = consul.Consul(host='54.152.246.15', port=8500)
 index = None
 
 
-index, data = c.kv.get('db_config', index=index)
-db_config = loads(data['Value'])
+index, data = c.kv.get('recommand_db_config', index=index)
+recommand_db_config = loads(data['Value'])
 
 
-conn = mariadb.connect(**db_config)
+conn = mariadb.connect(**recommand_db_config)
 cursor = conn.cursor()
 
 # 추천 종목 API
@@ -25,15 +25,16 @@ class Recommand(Resource):
     def get(self):
 
         max = """
-        SELECT stock_id FROM Recommands WHERE similarity=(SELECT MAX(similarity) FROM Recommands ORDER BY updated_time DESC LIMIT 10)
+        SELECT stock_id FROM Recommands WHERE similarity=(SELECT MAX(similarity) FROM Recommands ORDER BY recommand_time DESC LIMIT 5)
         """
         min = """
-        SELECT stock_id FROM Recommands WHERE similarity=(SELECT MIN(similarity) FROM Recommands ORDER BY updated_time DESC LIMIT 10)
+        SELECT stock_id FROM Recommands WHERE similarity=(SELECT MIN(similarity) FROM Recommands ORDER BY recommand_time DESC LIMIT 5)
         """
 
         cursor.execute(max)
         diff = cursor.fetchone()[0]
         cursor.execute(min)
         sim = cursor.fetchone()[0]
-        json_data = {"max": diff, "min": sim}
+        stock_code=["000660","005380","005490","005930","035720"]
+        json_data = {"max": stock_code[diff-1], "min": stock_code[sim-1]}
         return Response(dumps(json_data), status=200, mimetype='application/json')
